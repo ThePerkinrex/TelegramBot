fa45f5a340afe402d1bc045412628bb52733f35a
ThePerkinrex <theperkinrex@gmail.com>
2017-10-24 18:50:11 +0200

    Update MessageHandler.py to be more generic

--- Bot.py
+++ Bot.py
@@ -1,54 +1,27 @@
-import requests
-
 import MessageHandler as MHandler
+import Utils
 
 
 url = "https://api.telegram.org/bot479189806:AAFgwI7drgzRoXTGnoCxgrFyfWcUxKP1Wlc/"
 
 
-def get_updates_json(request, offset=None):
-    if offset is None:
-        response = requests.get(request + 'getUpdates')
-        return response.json()
-    response = requests.get(request + 'getUpdates?offset=' + str(offset))
-    return response.json()
-
-
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    if total_updates == -1:
-        return None
-    return results[total_updates]
-
-
-def get_chat_id(update):
-    if update is None:
-        return 0;
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
-
-
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Telegram Bot On')
+chat_id = Utils.get_chat_id(Utils.last_update(Utils.get_updates_json(url)))
+Utils.send_mess(chat_id, 'Telegram Bot On')
 
 
 def main():
-    if last_update(get_updates_json(url)) is None:
+    if Utils.last_update(Utils.get_updates_json(url)) is None:
         update_id = 1
     else:
-        update_id = last_update(get_updates_json(url))['update_id']
+        update_id = Utils.last_update(Utils.get_updates_json(url))['update_id']
     while True:
-        update = last_update(get_updates_json(url, update_id))
+        update = Utils.last_update(Utils.get_updates_json(url, update_id))
         if update is not None:
-            MHandler.handleMessage(update)
-            update_id = last_update(get_updates_json(url))['update_id'] + 1
+            Utils.send_mess(Utils.get_chat_id(update), MHandler.handleMessage(update))
+            mess = MHandler.handleMessage(update)
+            for message in mess:
+                Utils.send_mess(Utils.get_chat_id(update), message)
+            update_id = Utils.last_update(Utils.get_updates_json(url))['update_id'] + 1
 
 
 if __name__ == '__main__':
--- MessageHandler.py
+++ MessageHandler.py
@@ -1,18 +1,21 @@
-import Bot
+import Utils
 
 
 def handleMessage(update):
     print(update)
 
+    r = 'Error'
+
     message = update['message']
     if message['text'] == '/stop':
-        Bot.send_mess(Bot.get_chat_id(update), 'Stopping Chatbot')
+        Utils.send_mess(Utils.get_chat_id(update), 'Stopping Chatbot')
         exit(0)
     else:
         if message['chat']['type'] == 'group':
             messageToSend = 'Hello ' + message['from']['first_name']
-            Bot.send_mess(Bot.get_chat_id(update), messageToSend)
-            Bot.send_mess(Bot.get_chat_id(update), 'You told me "' + message['text'] + '"')
+            r = ['Hello ' + message['from']['first_name'], 'You told me "' + message['text'] + '"']
         else:
-            Bot.send_mess(Bot.get_chat_id(update), 'You told me "' + message['text'] + '"')
+            r = [Utils.get_chat_id(update), 'You told me "' + message['text'] + '"']
             print('Answering ' + str(update['update_id']))
+
+    return r
