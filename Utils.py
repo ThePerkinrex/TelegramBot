892cd7347035e1255b0279c667668532b6bbef9a
ThePerkinrex <theperkinrex@gmail.com>
2017-10-24 19:27:47 +0200

    Add custom keyboard functions

--- Utils.py
+++ Utils.py
@@ -26,7 +26,45 @@ def get_chat_id(update):
     return chat_id
 
 
-def send_mess(chat, text):
+def send_mess(chat, text, custom=None):
     params = {'chat_id': chat, 'text': text}
+    if custom is not None:
+        params['reply_markup'] = custom
     response = requests.post(url + 'sendMessage', data=params)
     return response
+
+
+"""
+get_custom_keyboard(buttons, custom=None):
+buttons: A list of rows represented by lists with keyboard_button dicts in the form of
+[[A1,A2,A3], # Row 1
+ [B1,B2,B3], # Row 2
+ [C1,C2,C3]] # Row 3
+ 
+Optionals:
+custom ==> A dict with custom options
+"""
+
+
+def get_custom_keyboard(buttons, custom=None):
+    r = {'keyboard': buttons}
+    if custom is not None:
+        r.update(custom)
+    return r
+
+
+"""
+get_custom_button(text, custom=None):
+text: the text of the button
+
+Optionals:
+custom ==> A dict with custom options
+"""
+
+
+def get_custom_button(text, custom=None):
+    r = {'text': text}
+    if custom is not None:
+        r.update(custom)
+
+    return r
